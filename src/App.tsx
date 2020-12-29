e6edc318d87f33f2e77f77bb909cb5bc8afa7237
Jaggler3 <martin.protostar@gmail.com>
2020-12-29 10:17:40 -0500

    new routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,19 @@ import '@fortawesome/fontawesome-free/css/all.css'
 
 import './App.scss';
 import Clean from './pages/Clean';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import Account from './pages/Account';
 
 function App() {
 	return (
-		<Clean />
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Clean} />
+				<Route exact path="/signup" component={SignUp} />
+				<Route exact path="/account" component={Account} />
+			</Switch>
+		</BrowserRouter>
 	);
 }
 
