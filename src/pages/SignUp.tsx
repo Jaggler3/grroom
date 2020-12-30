ff537965a77328e85acca7579f7138ab003cbcf1
Jaggler3 <martin.protostar@gmail.com>
2020-12-29 22:47:14 -0500

    updates

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,7 +2,46 @@ import React from 'react'
 
 export default function SignUp() {
 	return (
-		<div>
+		<div id="signup">
+			<div id="header">
+				<p className="service">Grroom</p>
+				<p className="title">Sign Up</p>
+			</div>
+			<p>Email</p>
+			<input type="text" />
+			<p>Password</p>
+			<input type="password" />
+			<p>Confirm Password</p>
+			<input type="password" />
+			<p>Select a plan</p>
+
+			<div>
+				<p>Free</p>
+				<p>10 projects</p>
+				<p>5KB max project size</p>
+				<p className="exclude">Presentations</p>
+			</div>
+
+			<div>
+				<p>$5/mo</p>
+				<p>10 projects</p>
+				<p>5KB max project size</p>
+				<p>Presentations</p>
+			</div>
+
+			<div className="most-popular">
+				<p>$9/mo</p>
+				<p>10 projects</p>
+				<p>5KB max project size</p>
+				<p>Presentations</p>
+			</div>
+
+			<div>
+				<p>$20/mo</p>
+				<p>10 projects</p>
+				<p>5KB max project size</p>
+				<p>Presentations</p>
+			</div>
 		</div>
 	)
 }
